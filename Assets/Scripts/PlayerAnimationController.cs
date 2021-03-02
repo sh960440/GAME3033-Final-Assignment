5dbafceaff20b3fceff62a8d79f68402858d4005
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-03-02 03:02:52 -0500

    Set player animation

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public bool isIdle, isWalking, isJumping, isStun;
+
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("idle", isIdle);
+        animator.SetBool("walk", isWalking);
+        animator.SetBool("jump", isJumping);
+        animator.SetBool("stun", isStun);
+        //animator.SetTrigger("idle");
+    }
+}
